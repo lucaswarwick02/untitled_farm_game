d57f5ad230760bd83691e0717dfacafeac4aff97
Lucas <lucaswarwick02@gmail.com>
2023-08-11 21:25:18 +0100

    Generate random island shape (not bound to save)

---
+++ Assets/Scripts/Utility/Map/IslandGenerator.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Utility.Map
+{
+    public static class IslandGenerator
+    {
+        public const int IslandSize = 300;
+        
+        public static int[,] GenerateIsland()
+        {
+            // 1. Probability map
+            var probabilityMap = new float[IslandSize, IslandSize];
+            for (var x = 0; x < IslandSize; x++)
+            {
+                for (var y = 0; y < IslandSize; y++)
+                {
+                    const float middle = IslandSize / 2f;
+                    var distanceFromMiddle = (Mathf.Abs(middle - x) + Mathf.Abs(middle - y)) / 2f;
+                    probabilityMap[x, y] = Mathf.Clamp01(1 - distanceFromMiddle / middle);
+                }
+            }
+            
+            // 2. Alive Threshold
+            var aliveMap = new int[IslandSize, IslandSize];
+            for (var x = 0; x < IslandSize; x++)
+            {
+                for (var y = 0; y < IslandSize; y++)
+                {
+                    if (Random.Range(0f, 1f) < probabilityMap[x, y]) aliveMap[x, y] = 1;
+                }
+            }
+            
+            // 3. Blur
+            var blurMap = new float[IslandSize, IslandSize];
+            for (var x = 0; x < IslandSize; x++)
+            {
+                for (var y = 0; y < IslandSize; y++)
+                {
+                    var neighbours = GetNeighbours(aliveMap, x, y, offset: 3);
+                    blurMap[x, y] = neighbours.Sum() / (float) neighbours.Length;
+                }
+            }
+            
+            // 4. Blur Threshold
+            var blurThresholdMap = new int[IslandSize, IslandSize];
+            for (var x = 0; x < IslandSize; x++)
+            {
+                for (var y = 0; y < IslandSize; y++)
+                {
+                    if (blurMap[x, y] > 0.5f) blurThresholdMap[x, y] = 1;
+                }
+            }
+
+            // 5. Smooth
+            var smoothMap = new int[IslandSize, IslandSize];
+            for (var iteration = 0; iteration < 5; iteration++)
+            {
+                for (var x = 0; x < IslandSize; x++)
+                {
+                    for (var y = 0; y < IslandSize; y++)
+                    {
+                        var neighbours = GetNeighbours(blurThresholdMap, x, y, offset: 1);
+
+                        if (blurThresholdMap[x, y] == 1)
+                        {
+                            // Cell is alive, check if it should die
+                            if (neighbours.Sum() < 3)
+                            {
+                                smoothMap[x, y] = 0;
+                            }
+                            else
+                            {
+                                smoothMap[x, y] = 1;
+                            }
+                        }
+                        else
+                        {
+                            if (neighbours.Sum() > 4)
+                            {
+                                smoothMap[x, y] = 1;
+                            }
+                            else
+                            {
+                                smoothMap[x, y] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+            var endTime = Time.timeAsDouble;
+
+            return smoothMap;
+        }
+        
+        private static int[] GetNeighbours(int[,] grid, int x, int y, int offset)
+        {
+            var neighbours = new List<int>();
+
+            for (var n_x = x - offset; n_x < x + offset + 1; n_x++)
+            {
+                for (var n_y = y - offset; n_y < y + offset + 1; n_y++)
+                {
+                    if (n_x is >= 0 and < IslandSize && n_y is >= 0 and < IslandSize)
+                    {
+                        neighbours.Add(grid[n_x, n_y]);
+                    }
+                }
+            }
+
+            return neighbours.ToArray();
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Map/MapBuilder.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Utility.Map
+{
+    public class MapBuilder : MonoBehaviour
+    {
+        [Header("Tilemaps")]
+        [SerializeField] private Tilemap grassTilemap;
+        [SerializeField] private Tilemap waterTilemap;
+        
+        [Header("Tiles")]
+        [SerializeField] private TileBase grassTile;
+        [SerializeField] private TileBase waterTile;
+
+        private static int WaterSize => IslandGenerator.IslandSize / 2 + 10;
+        
+        private void Start()
+        {
+            var island = IslandGenerator.GenerateIsland();
+
+            for (var x = 0; x < IslandGenerator.IslandSize; x++)
+            {
+                for (var y = 0; y < IslandGenerator.IslandSize; y++)
+                {
+                    if (island[x, y] == 1) grassTilemap.SetTile(IslandArrayToTilemap(x, y), grassTile);
+                }
+            }
+
+            for (var x = -WaterSize; x < WaterSize; x++)
+            {
+                for (var y = -WaterSize; y < WaterSize; y++)
+                {
+                    waterTilemap.SetTile(new Vector3Int(x, y), waterTile);
+                }
+            }
+        }
+
+        private static Vector3Int IslandArrayToTilemap(int x, int y)
+        {
+            return new Vector3Int(x - IslandGenerator.IslandSize / 2, y - IslandGenerator.IslandSize / 2);
+        }
+    }
+}
